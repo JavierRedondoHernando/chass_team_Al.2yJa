671a204555c72751aa16102df1f1bd0966191f4d
JavierRedondoHernando <36537926+JavierRedondoHernando@users.noreply.github.com>
2018-03-16 13:40:25 +0100

    Update alfil.c

--- alfil.c
+++ alfil.c
@@ -1,23 +1,23 @@
-#include <stdio.h> //recomiendo usas desde aqui...
+#include <stdio.h> 
 #define DIM2 9
 void main() {
 	int piezas[DIM2][DIM2];
 	int x, x1;
 	char y_0, y1_0;
-	int y, y1; //hacen falta para poder hacer las cuentas ya que recogemos un char pero queremos el int
+	int y, y1;
 	for (x = 0; x < DIM2; x++) {
 		for (y = 0; y < DIM2; y++) {
 				piezas[x][y] = 0;
 		}
 	}
-	piezas[8][8] = 2;//esta es solo la posición del alfil
+	piezas[8][8] = 2;
 	while (1) {
-		scanf_s("%d %c", &x, &y_0);//posicion de la pieza en las coordenadas de ajedrez
-		scanf_s("%d %c", &x1, &y1_0);//la posicion de ajedrez a la que te lo quieres llevar
-		y = y_0 - 'a' + 1;//transforma la y deletra a la coordenada que queremos
+		scanf_s("%d %c", &x, &y_0);
+		scanf_s("%d %c", &x1, &y1_0);
+		y = y_0 - 'a' + 1;
 		y1 = y1_0 - 'a' + 1;
-		//...hasta aqui para todas las piezas, es solo la lógica para llamar a los elementos de la matriz
-		if ((piezas [9 - x][y] == 2) && ((x - x1)*(x - x1) == (y - y1)*(y - y1))) {//este condicional es basicamente toda la logica del alfil
+		
+		if ((piezas [9 - x][y] == 2) && ((x - x1)*(x - x1) == (y - y1)*(y - y1))) {
 			piezas[9 - x][y] = 0;
 			piezas[9 - x1][y1] = 2;
 		}
